263bb3588c5dae031ad3990ea88164bac134d973
idkidontcare612 <VoidMarlee24@gmail.com>
2026-08-19 15:15:40 -0500

    Change bare URL in uv.config.js to new endpoint

--- active/uv/uv.config.js
+++ active/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/active/go/',
-    bare:'https://interstellar.click',
+    bare:'wss://wisp.mercury.net.za/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/active/uv/uv.handler.js',
